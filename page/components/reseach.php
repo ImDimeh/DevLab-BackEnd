572705265c8e36dbbb5b4c88997662b5d4b62032
MEHDI BELLAM <porcepic91@gmail.com>
2022-12-11 13:50:24 +0100

    ajout du components orderBy.php

--- page/components/reseach.php
+++ page/components/reseach.php
@@ -4,6 +4,7 @@
     <input type="search" name="moovie" id="moovie_Id" placeholder="chercher votre film" >
 
 </form>
+<script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
 <script>
     const recherche = document.getElementById("moovie_Id")
     console.log(recherche)
@@ -12,7 +13,21 @@
 
 recherche.addEventListener("input" , e => {
     const value = e.target.value
-    console.log(value)
+
+    req = "https://api.themoviedb.org/3/search/movie?api_key=cecfdcc4951a5a9c7eed2dd44b519117&query=" + value
+    req = req.replaceAll(" " , "+" )
+    console.log(req)
+    axios.get(req, {}).then(function (response) {
+        const datas = response.data.results
+        console.log(datas);
+
+    })
+        .catch(function (error) {
+            console.log(error);
+        })
+        .then(function () {
+            console.log("fin de la requette ")
+        });
 })
 
 
