b418a79208571599bf8572de3769ed22b7cff07a
MEHDI BELLAM <porcepic91@gmail.com>
2022-12-24 12:48:18 +0100

    ajout du OnClick sur les boutton de genre

--- page/components/function.php
+++ page/components/function.php
@@ -19,10 +19,14 @@
                     const GenreName = data.name
                     genre.innerText = GenreName ;
                     
-                    genre.onclick = GetDataByLink('https://api.themoviedb.org/3/discover/movie?api_key=cecfdcc4951a5a9c7eed2dd44b519117&language=fr&sort_by=title.asc&page=1&with_genres=' + data.id)
+                    
+                  //  genre.onclick ="GetDataByLink('https://api.themoviedb.org/3/discover/movie?api_key=cecfdcc4951a5a9c7eed2dd44b519117&language=fr&sort_by=title.asc&page=1&with_genres=' + data.id)"
 
+                  genre.setAttribute('OnClick' , "GetDataByLink('https://api.themoviedb.org/3/discover/movie?api_key=cecfdcc4951a5a9c7eed2dd44b519117&language=fr&sort_by=title.asc&page=1&with_genres=" + data.id + ")" ) 
                     genre.setAttribute('id',data.id);
+                   // genre.setAttribute('onclick','doSomething();')
                     console.log(genre)
+                    
                     //https://api.themoviedb.org/3/discover/movie?api_key=###&with_genres=28
                     
 
